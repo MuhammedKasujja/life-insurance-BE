7748681426e7ec84b681112f44518ed6cb6a4fb0
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-07-13 01:14:57 +0300

    feat: added dto validation messages

--- src/recommendation/dto/create-profile.dto.ts
+++ src/recommendation/dto/create-profile.dto.ts
@@ -7,10 +7,25 @@ const riskToleranceValues = Object.values(RiskTolerance);
 
 export const CreateProfileSchema = extendApi(
   z.object({
-    age: z.coerce.number().min(15).max(90),
-    income: z.coerce.number().min(500).max(1_000_000),
-    numOfDependants: z.coerce.number().max(10, 'Dependants must not exceed 10'),
-    riskTolerance: z.enum(riskToleranceValues as [string, ...string[]]),
+    age: z.coerce
+      .number({ required_error: 'Age is required', message: 'Age is required' })
+      .min(15, 'Only 15 and above are eligible')
+      .max(90, 'Not eligible'),
+    income: z.coerce
+      .number({
+        required_error: 'Monthly income is required',
+        message: 'Monthly income is required',
+      })
+      .min(500)
+      .max(1_000_000),
+    numOfDependants: z.coerce
+      .number()
+      .max(10, 'Dependants must not exceed 10')
+      .optional(),
+    riskTolerance: z.enum(riskToleranceValues as [string, ...string[]], {
+      required_error: 'Field is required',
+      message: 'Field is required',
+    }),
   }),
   {
     title: 'Recommendation',
